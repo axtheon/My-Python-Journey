1d3c7f0267cb6037059f69573e535701bbefd692
axtheon <axtheon>
2025-07-03 09:49:14 -0700

    Learnt about Dictionaries and their methods

--- chapter 5/01_dict.py
+++ chapter 5/01_dict.py
@@ -3,7 +3,18 @@
 # use a list.
 marks = {
     "Abdullah": 100,
-    "Ali": 94,
+    "Ali": 84,
     "Ahmed": 26
 }
 print(marks, type(marks))
+# In dictionaries, we cant use indexing to print a single value
+# we have to use the name of the key.
+# Like if you want to print marks of Ali:
+print("Marks of Ali are: ", + marks["Ali"])
+# Properties of Dictionaries:
+# They are mutable
+# They are unordered
+# They are already indexed in the code means it will not go through all
+# The values in it instead it will directly go to the value we want to
+# print. So it's faster than lists, tuples etc.
+# They cannot contain duplicate keys.
---
+++ chapter 5/02_dict_methods.py
@@ -0,0 +1,24 @@
+# Some of the methods for Dictionaries are following:
+marks = {
+    "Abdullah": 100,
+    "Ali": 84,
+    "Ahmed": 26
+}
+print(marks.items()) # will print items in the dict separately
+print(marks.keys()) # will print the keys used in dict
+print(marks.values()) # will print the values of the keys in the dict
+marks.update({"Abdullah": 95}) # will change the value of a key
+print(marks)
+# By using marks.update method we can also add a new key in the existing dictionary.
+marks.update({"Hamza": 100})
+print(marks)
+print(marks.get("Abdullah")) # will print the marks of Abdullah
+'''
+In the previous file I used print(marks["Abdullah"]) and in this file I am using
+print(marks.get("Abdullah")) this method. Yeah these are both used to do the same task
+but if I used a key which doesn't exist in the dictionary the .get method will output
+None but if we use print(marks["Abdullah"]) this method it will provide us an error.
+FOR EXAMPLE:
+print(marks.get("Ayesha")) : Output: None
+print(marks["Ayesha"]) : Output: Error
+'''
