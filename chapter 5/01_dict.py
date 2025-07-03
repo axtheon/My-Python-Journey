3c56e87c27b5cd6f3c09a919c76a11e57240ea9d
axtheon <axtheon>
2025-07-03 09:04:57 -0700

    renamed

---
+++ chapter 5/01_dict.py
@@ -0,0 +1,9 @@
+# A dictionary is same as a list but if you want that
+# key values should associate with each other you can
+# use a list.
+marks = {
+    "Abdullah": 100,
+    "Ali": 94,
+    "Ahmed": 26
+}
+print(marks, type(marks))
